f5aef1a273171e38d67b37b9aeafc93359c8bd95
Matias Villar <matiase.villar@gmail.com>
2024-04-09 16:12:46 -0400

    WebHooksClerk

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-  publicRoutes: ['/', '/api/webhook/clerk', '/api/uploadthing'],
+  publicRoutes: ['/', '/api/webhook(.*)', '/api/uploadthing'],
   ignoredRoutes: ['/api/webhook/clerk'],
 });
 
