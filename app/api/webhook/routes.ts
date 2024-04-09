b01d0458b5563de930e3ce73326ffc3c72e22bb2
Matias Villar <matiase.villar@gmail.com>
2024-04-09 16:46:47 -0400

    .

--- app/api/webhook/routes.ts
+++ app/api/webhook/routes.ts
@@ -1,59 +1,88 @@
-import { Webhook } from 'svix';
+import { Webhook, WebhookRequiredHeaders } from 'svix';
 import { headers } from 'next/headers';
-import { WebhookEvent } from '@clerk/nextjs/server';
 
-export async function POST(req: Request) {
-  // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+import { IncomingHttpHeaders } from 'http';
 
-  if (!WEBHOOK_SECRET) {
-    throw new Error(
-      'Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local'
-    );
-  }
+import { NextResponse } from 'next/server';
+import { updateUser, fetchUserInfo } from '@/lib/actions/user.actions';
 
-  // Get the headers
-  const headerPayload = headers();
-  const svix_id = headerPayload.get('svix-id');
-  const svix_timestamp = headerPayload.get('svix-timestamp');
-  const svix_signature = headerPayload.get('svix-signature');
+type EventType = 'user.created' | 'user.updated';
 
-  // If there are no headers, error out
-  if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response('Error occured -- no svix headers', {
-      status: 400,
-    });
-  }
+type Event = {
+  data: Record<string, string | number | Record<string, string>[]>;
+  object: 'event';
+  type: EventType;
+};
+export const POST = async (request: Request) => {
+  const payload = await request.json();
+  const header = headers();
 
-  // Get the body
-  const payload = await req.json();
-  const body = JSON.stringify(payload);
+  const heads = {
+    'svix-id': header.get('svix-id'),
+    'svix-timestamp': header.get('svix-timestamp'),
+    'svix-signature': header.get('svix-signature'),
+  };
 
-  // Create a new Svix instance with your secret.
-  const wh = new Webhook(WEBHOOK_SECRET);
+  // Activitate Webhook in the Clerk Dashboard.
+  // After adding the endpoint, you'll see the secret on the right side.
+  const wh = new Webhook(process.env.WEBHOOK_SECRET || '');
 
-  let evt: WebhookEvent;
+  let evnt: Event | null = null;
 
-  // Verify the payload with the headers
   try {
-    evt = wh.verify(body, {
-      'svix-id': svix_id,
-      'svix-timestamp': svix_timestamp,
-      'svix-signature': svix_signature,
-    }) as WebhookEvent;
+    evnt = wh.verify(
+      JSON.stringify(payload),
+      heads as IncomingHttpHeaders & WebhookRequiredHeaders
+    ) as Event;
   } catch (err) {
-    console.error('Error verifying webhook:', err);
-    return new Response('Error occured', {
-      status: 400,
-    });
+    return NextResponse.json({ message: err }, { status: 400 });
   }
 
-  // Get the ID and type
-  const { id } = evt.data;
-  const eventType = evt.type;
+  const eventType: EventType = evnt?.type!;
 
-  console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
-  console.log('Webhook body:', body);
+  // Listen user creation event
+  if (eventType === 'user.created') {
+    // Resource: https://clerk.com/docs/reference/backend-api/tag/users#operation/Createuser
+    // Show what evnt?.data sends from above resource
+    const { id, username, name, userImg, folders, isOnboard } =
+      evnt?.data ?? {};
 
-  return new Response('', { status: 200 });
-}
+    try {
+      // @ts-ignore
+      await updateUser(
+        // @ts-ignore
+        { id, username, name, userImg, folders, isOnboard }
+      );
+
+      return NextResponse.json({ message: 'User created' }, { status: 201 });
+    } catch (err) {
+      console.log(err);
+      return NextResponse.json(
+        { message: 'Internal Server Error' },
+        { status: 500 }
+      );
+    }
+  }
+
+  // Listen organization updation event
+  if (eventType === 'user.updated') {
+    try {
+      // Resource: https://clerk.com/docs/reference/backend-api/tag/Organizations#operation/UpdateOrganization
+      // Show what evnt?.data sends from above resource
+      const { id, username, name, userImg, folders, isOnboard } = evnt?.data;
+      console.log('updated', evnt?.data);
+
+      // @ts-ignore
+      await updateUser({ id, username, name, userImg, folders, isOnboard });
+
+      return NextResponse.json({ message: 'Member Updated' }, { status: 201 });
+    } catch (err) {
+      console.log(err);
+
+      return NextResponse.json(
+        { message: 'Internal Server Error' },
+        { status: 500 }
+      );
+    }
+  }
+};
